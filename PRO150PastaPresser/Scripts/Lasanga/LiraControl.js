09f6c8116db5847ad82bcb802327ac8b5949403d
CuddlySpartan <ftruso99@gmail.com>
2018-11-13 15:42:22 -0700

    Updated JS to do buttons work (bug: new button break thing)

--- PRO150PastaPresser/Scripts/Lasanga/LiraControl.js
+++ PRO150PastaPresser/Scripts/Lasanga/LiraControl.js
@@ -1,9 +1,50 @@
-﻿window.addEventListener('load', function() {
+﻿//lasagna click
+window.addEventListener('load', function () {
     let trash = document.getElementById('trash');
     let number = document.getElementById('number');
     let numberLiteral = parseInt(number.innerHTML);
     trash.onclick = () => {
         numberLiteral++;
+        number.innerText = numberLiteral;
+    }
+});
+//cheese button
+window.addEventListener('load', function () {
+    let upgrade = document.getElementById('upgradeCheese');
+    let number = document.getElementById('lpsNum');
+    let numberLiteral = parseInt(number.innerHTML);
+    upgrade.onclick = () => {
+        numberLiteral+=2;
+        number.innerText = numberLiteral;
+    }
+});
+//sauce button
+window.addEventListener('load', function () {
+    let upgrade = document.getElementById('upgradeSauce');
+    let number = document.getElementById('lpsNum');
+    let numberLiteral = parseInt(number.innerHTML);
+    upgrade.onclick = () => {
+        numberLiteral += 3;
+        number.innerText = numberLiteral;
+    }
+});
+//pasta button
+window.addEventListener('load', function () {
+    let upgrade = document.getElementById('upgradePasta');
+    let number = document.getElementById('lpsNum');
+    let numberLiteral = parseInt(number.innerHTML);
+    upgrade.onclick = () => {
+        numberLiteral += 5;
+        number.innerText = numberLiteral;
+    }
+});
+//meat button
+window.addEventListener('load', function () {
+    let upgrade = document.getElementById('upgradeMeat');
+    let number = document.getElementById('lpsNum');
+    let numberLiteral = parseInt(number.innerHTML);
+    upgrade.onclick = () => {
+        numberLiteral += 10;
         number.innerText = numberLiteral;
     }
 });
