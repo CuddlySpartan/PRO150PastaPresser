0159dcd1098d1ec8c9b2f8c75c90c83b518945a6
BGnger <bthompson@student.neumont.edu>
2018-11-13 18:25:33 -0700

    Attempted to add Amount of Lira to system

--- PRO150PastaPresser/Scripts/Lasanga/LiraControl.js
+++ PRO150PastaPresser/Scripts/Lasanga/LiraControl.js
@@ -15,15 +15,18 @@ window.addEventListener('load', function () {
     let ccount = document.getElementById('cheeseCount');
     let numberLiteral = parseInt(number.innerHTML);
     let ccountLiteral = parseInt(ccount.innerHTML);
+    cupgrade.children.namedItem('UpgradeLabel').innerText = calculateAmountOfLira(ccountLiteral, 1.05);
     cupgrade.onclick = () => {
         numberLiteral += 2;
         ccountLiteral++;
         ccount.innerText = ccountLiteral;
         number.innerText = numberLiteral;
+        cupgrade.innerText = raiseLiraLevel(cupgrade);
     }
     let supgrade = document.getElementById('upgradeSauce');
     let scount = document.getElementById('sauceCount');
     let scountLiteral = parseInt(scount.innerHTML);
+    supgrade.children.namedItem('UpgradeLabel').innerText = calculateAmountOfLira(scountLiteral, 1.05);
     supgrade.onclick = () => {
         numberLiteral += 3;
         scountLiteral++;
@@ -33,6 +36,7 @@ window.addEventListener('load', function () {
     let pupgrade = document.getElementById('upgradePasta');
     let pcount = document.getElementById('pastaCount');
     let pcountLiteral = parseInt(pcount.innerHTML);
+    pupgrade.children.namedItem('UpgradeLabel').innerText = calculateAmountOfLira(pcountLiteral, 1.05);
     pupgrade.onclick = () => {
         numberLiteral += 5;
         pcountLiteral++;
@@ -42,6 +46,7 @@ window.addEventListener('load', function () {
     let mupgrade = document.getElementById('upgradeMeat');
     let mcount = document.getElementById('meatCount');
     let mcountLiteral = parseInt(mcount.innerHTML);
+    pupgrade.children.namedItem('UpgradeLabel').innerText = calculateAmountOfLira(pcountLiteral, 1.05);
     mupgrade.onclick = () => {
         numberLiteral += 10;
         mcountLiteral++;
@@ -49,3 +54,18 @@ window.addEventListener('load', function () {
         number.innerText = numberLiteral;
     }
 });
+
+function calculateAmountOfLira(amountOfLevels, levelRateOfInterest) {
+    let num, rate; 
+    num = ParseInt(amountOfLevels.innerHTML);
+    rate = ParseInt(levelRateOfInterest.innerHTML);
+    rate++;
+    for (var i = 0; i < num; i++) {
+        num *= rate;
+    }
+    return num;
+}
+
+function raiseLiraLevel() {
+
+}
